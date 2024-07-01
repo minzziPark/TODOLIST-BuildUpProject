62270f018eb9058f44cafd0b7ccd4676501aab05
minzziPark <minzzi0068@gmail.com>
2024-07-01 17:56:21 +0900

    fix : month 오류 수정

--- todolist_vanilaJS/frontend/static/js/date.js
+++ todolist_vanilaJS/frontend/static/js/date.js
@@ -5,7 +5,7 @@ const days = ["SUN", "MON", "TUE", "THU", "FRI", "SAT"];
 function getDate() {
   const dateObj = new Date();
   const year = String(dateObj.getFullYear());
-  const month = String(dateObj.getMonth()).padStart(2, "0");
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
   const day = String(dateObj.getDate()).padStart(2, "0");
   const dayOfWeek = String(dateObj.getDay());
   date.innerText = `${year}.${month}.${day} ${days[dayOfWeek]}`;
